1d9cb68832d3bde74d26852b277173e9dc1b7312
MCDFSteve <2911898435@qq.com>
2026-09-25 06:00:46 +0800

    写明视频链接播放入口

--- plugins/custom.web_media_source/custom.web_media_source.js
+++ plugins/custom.web_media_source/custom.web_media_source.js
@@ -3,7 +3,7 @@ const pluginManifest = {
   name: '哔哩哔哩链接播放',
   version: '1.0.0',
   minHostVersion: '1.11.8',
-  description: '在视频播放页粘贴哔哩哔哩链接播放，支持分 P 选择和按标题搜索弹幕',
+  description: '让 NipaPlay 支持播放哔哩哔哩视频。使用方法：在“视频播放”页面点击“输入链接”，粘贴哔哩哔哩视频链接后点击“播放链接”。',
   author: 'NipaPlay',
   permissions: ['url.resolve'],
   priority: 50
